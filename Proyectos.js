ae3759ddf0f366d89732171aa1e4e5f0ba047b8e
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-10-23 21:54:22 -0300

    feat portafolio

---
+++ Proyectos.js
@@ -0,0 +1,66 @@
+
+export default {  bloques: [
+  `<li id="lister-card-proyect">
+<img class="item-lista-img" src="./img/Movie.png" alt="movie">
+<div id="enlaces">
+  <a href="#">
+    <p id="enlaces-text">
+      <span class="icono-search"></span>
+      <span class="enlaces-title">Movie</span>
+      desarrollada con
+      <span class="skill-enlace">
+       react, react-redux
+       </span>
+    </p>
+  </a>    
+</div>
+</li>`,
+  `<li id="lister-card-proyect">
+<img  class="item-lista-img" src="./img/dogs.png" alt="movie">
+<div id="enlaces">
+  <a href="#">
+    <p id="enlaces-text">
+      <span class="icono-search"></span>
+      <span class="enlaces-title">Dog</span>
+      desarrollada con
+      <span class="skill-enlace">
+      react, react-redux, sql, express, node.js 
+      </span>
+    </p>
+  </a>   
+</div>
+</li>
+`,
+
+  `<li id="lister-card-proyect">
+<img  class="item-lista-img" src="./img/pokemos.png" alt="movie">
+<div id="enlaces">
+  <a href="#">
+    <p id="enlaces-text">
+      <span class="icono-search"></span>
+      <span class="enlaces-title">Pokedex</span>
+      desarrollada con
+      <span class="skill-enlace">
+       react, react-redux, sql, express, node.js, syled-componet 
+      </span>
+    </p>
+  </a>   
+</div>
+</li>`,
+  `<li id="lister-card-proyect">
+<img  class="item-lista-img" src="./img/fit-focus.png" alt="fit focus">
+<div id="enlaces">
+  <a href="#">
+    <p id="enlaces-text">
+      <span class="icono-search"></span>
+      <span class="enlaces-title">Fit Focus</span>
+      fit focus es una app de ejercicios desarrolla con las siguientes tecnologias:
+      <span class="skill-enlace">
+        react, react native, typescript, redux toolkit, mongoose, mongodb, tailwind, node.js
+      </span>
+    </p>
+  </a>   
+</div>
+</li>`
+]
+}
