a7bc6f7493995b40f022961ecc87fa1d4451346d
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-10-27 16:05:45 -0300

    styles

--- Proyectos.js
+++ Proyectos.js
@@ -1,26 +1,38 @@
-
-export default {  bloques: [
-  `<li id="lister-card-proyect">
-<img class="item-lista-img" src="./img/Movie.png" alt="movie">
-<div id="enlaces">
-  <a href="#">
-    <p id="enlaces-text">
-      <span class="icono-search"></span>
-      <span class="enlaces-title">Movie</span>
-      <p id="language-spa">desarrollada con</p>
-      <span class="skill-enlace">
-       react, react-redux
-       </span>
-    </p>
-  </a>    
-</div>
+export default {
+  bloques: [
+`<li id="lister-card-proyect">
+  <img class="item-lista-img" src="./img/Movie.png" alt="movie">
+  <div id="enlaces">
+      <p id="enlaces-text">
+        <div class="icon_enlaces">
+          <a href="#" class="icon_repo" target="_blank">
+              <img src="./img/icons8-github.svg">
+          </a>
+          <a href="https://pf-app-five.vercel.app/" class="icon_link" target="_blank">
+              <img src="https://img.icons8.com/nolan/344/1A6DFF/C822FF/link.png">
+          </a>
+        </div>
+        <span class="enlaces-title">Movie</span>
+        <p id="language-spa">desarrollada con</p>
+        <span class="skill-enlace">
+        react, react-redux
+        </span>
+      </p>
+    </a>    
+  </div>
 </li>`,
-  `<li id="lister-card-proyect">
-<img  class="item-lista-img" src="./img/dogs.png" alt="movie">
-<div id="enlaces">
-  <a href="#">
-    <p id="enlaces-text">
-      <span class="icono-search"></span>
+`<li id="lister-card-proyect">
+    <img  class="item-lista-img" src="./img/dogs.png" alt="dog">
+    <div id="enlaces">
+   <p id="enlaces-text">
+      <div class="icon_enlaces">
+        <a href="#" class="icon_repo" target="_blank">
+            <img src="./img/icons8-github.svg">
+        </a>
+        <a href="https://pf-app-five.vercel.app/" class="icon_link" target="_blank">
+            <img src="https://img.icons8.com/nolan/344/1A6DFF/C822FF/link.png">
+        </a>
+      </div>
       <span class="enlaces-title">Dog</span>
       <p id="language-spa">desarrollada con</p>
       <span class="skill-enlace">
@@ -32,12 +44,18 @@ export default {  bloques: [
 </li>
 `,
 
-  `<li id="lister-card-proyect">
-<img  class="item-lista-img" src="./img/pokemos.png" alt="movie">
-<div id="enlaces">
-  <a href="#">
+`<li id="lister-card-proyect">
+    <img  class="item-lista-img" src="./img/pokemos.png" alt="pokem">
+    <div id="enlaces">
     <p id="enlaces-text">
-      <span class="icono-search"></span>
+      <div class="icon_enlaces">
+        <a href="#" class="icon_repo" target="_blank">
+            <img src="./img/icons8-github.svg">
+        </a>
+        <a href="https://pf-app-five.vercel.app/" class="icon_link" target="_blank">
+            <img src="https://img.icons8.com/nolan/344/1A6DFF/C822FF/link.png">
+        </a>
+      </div>
       <span class="enlaces-title">Pokedex</span>
       <p id="language-spa">desarrollada con</p>
       <span class="skill-enlace">
@@ -47,20 +65,25 @@ export default {  bloques: [
   </a>   
 </div>
 </li>`,
-  `<li id="lister-card-proyect">
-<img  class="item-lista-img" src="./img/fit-focus.png" alt="fit focus">
-<div id="enlaces">
-  <a href="#">
+`<li id="lister-card-proyect">
+    <img  class="item-lista-img" src="./img/fit-focus.png" alt="fit focus"> 
+    <div id="enlaces">
+    <div class="icon_enlaces">
+      <a href="#" class="icon_repo" target="_blank">
+          <img src="./img/icons8-github.svg" >
+      </a>
+      <a href="https://pf-app-five.vercel.app/" class="icon_link" target="_blank">
+          <img src="https://img.icons8.com/nolan/344/1A6DFF/C822FF/link.png">
+      </a>
+    </div>
     <p id="enlaces-text">
-      <span class="icono-search"></span>
       <span class="enlaces-title">Fit Focus</span>
       <p id="language-spa">fit focus es una app de ejercicios desarrolla con las siguientes tecnologias:</p>
       <span class="skill-enlace">
         react, react native, typescript, redux toolkit, mongoose, mongodb, tailwind, node.js
       </span>
     </p>
-  </a>   
 </div>
-</li>`
-]
-}
+</li>`,
+  ],
+};
